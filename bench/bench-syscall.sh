ae8cc1abde17e31bbe49515a0469beec3f35a124
Benshan Mei <mbs2021@163.com>
2024-05-01 05:51:17 +0800

    Add results.

---
+++ bench/bench-syscall.sh
@@ -0,0 +1,8 @@
+# Benchmark Parameters
+# open Open a text file with read and write permissions
+# read Read 10 KB from a file to a memory-mapped region
+# write Write 10 KB from a memory-mapped region to a file
+# mmap Map a 10KB region using the NULL file descriptor
+# munmap Unmap the 10KB region previously-mapped
+# socket Open a socket using AF_INET and SOCKSTREAM
+# printf Print a "Hello World!" message to the console
